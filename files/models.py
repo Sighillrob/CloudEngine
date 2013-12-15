12d48022cc49a9c0cef99a1abd69f194350b82e0
Swapnil Talekar <swapnil.st@gmail.com>
2013-12-15 16:17:53 +0530

    reference settings from django.conf

--- files/models.py
+++ files/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from core.models import CloudApp
-from cloudengine.settings import REMOTE_FILES_DIR
+from django.conf import settings
 
 
 class CloudFile(models.Model):
     name = models.CharField(max_length=200)
-    content = models.FileField(upload_to=REMOTE_FILES_DIR)
+    content = models.FileField(upload_to=settings.REMOTE_FILES_DIR)
     url = models.CharField(max_length=2000)
     size = models.BigIntegerField()
     app = models.ForeignKey(CloudApp)
