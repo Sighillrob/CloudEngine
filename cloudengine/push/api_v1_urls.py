fb19c6f5e6d12de2637ae993ea58918dc73fb415
Swapnil Talekar <swapnil.st@gmail.com>
2014-02-28 22:51:47 +0530

    separated api from views

--- cloudengine/push/api_v1_urls.py
+++ cloudengine/push/api_v1_urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
-from cloudengine.push.views import PushAPIView, PushSubscribers
+from cloudengine.push.api import PushAPIView, PushSubscribers
 
 urlpatterns = patterns('',
                        url(
