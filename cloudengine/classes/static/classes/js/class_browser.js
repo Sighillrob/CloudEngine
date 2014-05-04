ba9ef8f0a3a01ae1c564df378e5334c057048f85
Swapnil Talekar <swapnil.st@gmail.com>
2014-05-05 00:00:45 +0530

    classes template update

---
+++ cloudengine/classes/static/classes/js/class_browser.js
@@ -0,0 +1,12 @@
+
+
+var classBrowserApp = angular.module('classBrowserApp', []);
+
+
+classBrowserApp.controller('classListCtrl', function classListCtrl($scope) {
+	
+	
+	
+	$scope.selectedApp = "Select an App";
+	
+})
