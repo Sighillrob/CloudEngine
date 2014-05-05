ec1dd6a11fdc20c84906c66ff9a35be18462d203
Swapnil Talekar <swapnil.st@gmail.com>
2014-05-05 10:31:00 +0530

    get apps list from server

--- cloudengine/classes/static/classes/js/class_browser.js
+++ cloudengine/classes/static/classes/js/class_browser.js
@@ -5,7 +5,7 @@ var classBrowserApp = angular.module('classBrowserApp', []);
 
 classBrowserApp.controller('classListCtrl', function classListCtrl($scope) {
 	
-	
+	update_apps_list($scope);
 	
 	$scope.selectedApp = "Select an App";
 	
