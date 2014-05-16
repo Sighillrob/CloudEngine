f25af061d54f2c9528607a6268b26f747cdbb94c
Swapnil Talekar <swapnil.st@gmail.com>
2014-05-17 03:10:50 +0530

    allow clients to override pagination/set page size

--- cloudengine/core/utils.py
+++ cloudengine/core/utils.py
@@ -1,11 +1,18 @@
 
 from django.core.paginator import (Paginator, 
                     EmptyPage, PageNotAnInteger)
-from django.conf import settings
+from rest_framework.settings import api_settings
 from rest_framework.pagination import PaginationSerializer
 
 def paginate(request, list_result):
-    paginator = Paginator(list_result, settings.PAGINATE_BY)
+    try:
+        page_size = request.QUERY_PARAMS['page_size']
+        page_size = int(page_size)
+        if not page_size:
+            return list_result
+    except KeyError:
+        page_size = api_settings.PAGINATE_BY  
+    paginator = Paginator(list_result, page_size)
     page = request.QUERY_PARAMS.get('page')
     try:
         objects = paginator.page(page)
